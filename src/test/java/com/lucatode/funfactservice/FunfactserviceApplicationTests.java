b108e73ea3a2411f5647c5b59feeb8b51242f2d5
unknown <l.todeschini88@gmail.com>
2018-06-02 15:00:42 +0200

    starting with spring

---
+++ src/test/java/com/lucatode/funfactservice/FunfactserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucatode.funfactservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunfactserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
